7478a8c34e92ca72254829dff1ab921da2eeec8d
MalarS-37 <malarvizhiii271@gmail.com>
2026-02-21 18:39:21 +0530

    Update api.py

--- api.py
+++ api.py
@@ -1,9 +1,10 @@
 import google.generativeai as genai
 
-genai.configure(api_key="AIzaSyBcE_Y325qlBW34YOeKLRvaQ9_y-xdFIdk")
+genai.configure(api_key="your_api_key")
 
 models = genai.list_models(models/gemini-2.5-flash)
 
 print("Available Gemini Models:\n")
 for model in models:
     print(model.name)
+
